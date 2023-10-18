61ea3a6f6dab93ce925ffb4357701cfa7740e300
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-10-18 14:57:58 +0600

    Completed setup for firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4nObZrUaw2tjJtx1b7ofZvnIi1NYwono",
+  authDomain: "dewy-drinks-client.firebaseapp.com",
+  projectId: "dewy-drinks-client",
+  storageBucket: "dewy-drinks-client.appspot.com",
+  messagingSenderId: "873828399103",
+  appId: "1:873828399103:web:26f9e91022d75e170d99ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
