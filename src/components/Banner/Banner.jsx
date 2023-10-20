50114d22f7925b6e5f049e8993d91015d2cab374
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-10-20 17:33:17 +0600

    updated the banner section style

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -11,7 +11,8 @@ const Banner = () => {
     return (
         <div className='relative'>
             <img className="lg:h-[800px] w-full" src={banner} alt="" />
-            <h2 data-aos="fade-down" data-aos-duration="2000" className=" text-2xl lg:text-5xl font-bold absolute top-4 lg:top-24 text-orange-600 left-10 lg:left-96">... Create more Happiness ... with our drinks ...</h2>
+            <h2 data-aos="fade-down" data-aos-duration="2000" className=" text-lg lg:text-5xl font-bold absolute top-4 lg:top-24 text-orange-600 left-1 md:left-10 lg:left-52">... Create more Happiness ...</h2>
+            <h2 data-aos="fade-down" data-aos-duration="2000" className=" text-lg lg:text-5xl font-bold absolute top-4 lg:top-24 text-orange-600 right-1 md:right-10 lg:right-52">... with our drinks ...</h2>
             <div data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0" className='bg-gradient-to-r from-orange-500 to-amber-400 absolute lg:-bottom-36 lg:right-24 lg:rounded-lg p-6 md:w-full lg:w-[500px]'>
                 <h1 className='text-xl lg:text-4xl font-bold text-white'>Crafting brands and choices people love</h1>
                 <p className='text-xl max-w-2xl text-center text-white border border-indigo-800 rounded-lg p-4 mt-6'>We are committed to offering people more fo the drinks they want across a range of categories and in a variety of size.</p>
